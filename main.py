965fdb096cdcb78e835799ce8cbeeb863b1449a3
Kartikrana001 <kr3511441@gmail.com>
2026-06-21 19:22:49 +0530

    initial tradetracker setup

---
+++ main.py
@@ -0,0 +1,11 @@
+while True:
+    print("")
+    user_choice = input("Enter your choise: ")
+    if user_choice == "1" :
+        print("add trade")
+    elif user_choice == "2":
+        print("view trades ")
+    elif user_choice == "3":
+        break
+    else:
+        print("invelid choice")

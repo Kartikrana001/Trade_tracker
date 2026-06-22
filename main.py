2e3c5c5f2776c29d1d3a2d359c1b53ad9b1962da
Kartikrana001 <kr3511441@gmail.com>
2026-06-22 20:51:12 +0530

    added add trade and view trade feature

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+trades = []
+def add_trade():
+    stock = input("Enter stock name: ").strip()
+    buy_price = float(input("Enter buy price: "))
+    trades.append({"stock":stock,"buy_price":buy_price})
+def view_trades():
+    for trade in trades:
+        print(trade["stock"],"-",trade["buy_price"])
 while True:
     print("=====TRADE TRACKER=====\n\nADD TRADE        : 1\nVIEW TRADES      : 2\nEXIT             : 3")
     user_choice = input("Enter your choise: ")
     if user_choice == "1" :
-        print("add trade")
+        add_trade()
     elif user_choice == "2":
-        print("view trades ")
+        view_trades()
     elif user_choice == "3":
         break
     else:
